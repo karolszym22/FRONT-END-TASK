f9b998dd17e3c04bbd604c60ded27b0ac298521e
Karol Szymański <karol2134@op.pl>
2022-05-06 17:24:16 +0200

    Formik implantation

--- src/EditIntern.jsx
+++ src/EditIntern.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { NavLink } from 'react-router-dom';
+import { Formik } from 'formik';
 
 const EditIntern = () => {
     const { id } = useParams();
@@ -17,15 +18,59 @@ const EditIntern = () => {
     return (
         <div>
             <NavLink to="/">Back to list </NavLink>
-            <form>
-                <label></label>
-                <input type="text" name="name" value={intern.name}  />              
-                <label>Email</label>
-                <input type="text" name="email" value={intern.email} />
-                <input type="date" id="start" name="trip-start"value="RRRR-MM-DD"min="" max=""></input>
-                <input type="date" id="start" name="trip-start"value="RRRR-MM-DD"min="" max=""></input>
-                <input type="submit" value="Submit" />
-            </form>
+            <Formik
+       initialValues={{ email: '', password: '' }}
+       validate={values => {
+         const errors = {};
+         if (!values.email) {
+           errors.email = 'Required';
+         } else if (
+           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+         ) {
+           errors.email = 'Invalid email address';
+         }
+         return errors;
+       }}
+       onSubmit={(values, { setSubmitting }) => {
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           setSubmitting(false);
+         }, 400);
+       }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         handleChange,
+         handleBlur,
+         handleSubmit,
+         isSubmitting,
+         /* and other goodies */
+       }) => (
+         <form onSubmit={handleSubmit}>
+           <input
+             type="text"
+             name="name"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={intern.name}
+           />
+           {errors.email && touched.email && errors.email}
+           <input
+             type="email"
+             name="email"
+             onChange={handleChange}
+             onBlur={handleBlur}
+             value={intern.email}
+           />
+           {errors.password && touched.password && errors.password}
+           <button type="submit" disabled={isSubmitting}>
+             Submit
+           </button>
+         </form>
+       )}
+     </Formik>
         </div>
     );
 };
