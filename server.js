ff353480c27fa57005565d7b603764e2120366bb
Karol Szymański <karol2134@op.pl>
2022-05-09 17:01:01 +0200

    Adds access to all roads

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post("/values", async (req, res) =>{
 })
 
 app.use(express.static (path.join(__dirname, 'build')));
-app.get('/', function(req, res){
+app.get('*', function(req, res){
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
