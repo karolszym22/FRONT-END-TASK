7341792e27dc56bd7964f3773c1f8d0a49405301
Karol Szymański <karol2134@op.pl>
2022-05-10 17:40:49 +0200

    Adding app = server connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import React, { Component } from 'react';
 import "./App.css";
 import EditIntern from "./EditIntern";
 import InternList from "./InternList";
 import { Routes, Route } from "react-router-dom";
 
-function App() {
+
+class App extends Component {
+  state = {
+      data: null
+    };
+  
+    componentDidMount() {
+      this.callBackendAPI()
+        .then(res => this.setState({ data: res.express }))
+        .catch(err => console.log(err));
+    }
+    callBackendAPI = async () => {
+      const response = await fetch('/express_backend');
+      const body = await response.json();
+  
+      if (response.status !== 200) {
+        throw Error(body.message) 
+      }
+      return body;
+    };
+render() {
   return (
     <div className="App">
       <Routes>
@@ -12,6 +33,6 @@ function App() {
       </Routes>
     </div>
   );
-}
+}}
 
 export default App;
