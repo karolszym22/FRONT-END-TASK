e3c9939d244adf3e2af4845ce75dbd90ca79bcfa
Karol Szymański <karol2134@op.pl>
2022-05-10 21:25:21 +0200

    Edit server.js

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express'); //Line 1
 const app = express(); //Line 2
-const port = process.env.PORT || 3000; //Line 3
+const port = process.env.PORT || 3002; //Line 3
+const bodyParser = require('body-parser');
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
-// This displays message that the server running and listening to specified port
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // create a GET route
 app.get('/express_backend', (req, res) => { //Line 9
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 }); //
+
+app.post('/data', async (req,res) => {
+  console.log('Request received from REACT ONE');    
+  console.log(req.body);
+  res.json('Transaction Sent');
+});
